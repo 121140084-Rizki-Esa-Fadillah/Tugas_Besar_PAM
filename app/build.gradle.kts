1f47102bbcc5090c7540c88228e8a861de4fec1d
dimassaputra059 <dimas.121140059@student.itera.ac.id>
2024-05-19 21:55:12 +0700

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,4 +98,15 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     kapt("com.github.bumptech.glide:compiler:4.16.0")
 
+    // Material Design
+    implementation("com.google.android.material:material:1.2.1")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
 }
